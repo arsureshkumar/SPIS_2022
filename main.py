05f204ac1d867a51fe01110ed3999e566b15a580
Arjun Suresh Kumar <Arjun Suresh Kumar>
2022-08-25 13:32:54 -0700

    import statements

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world")
+from keras.preprocessing.text import Tokenizer
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Embedding
+import pandas as pd
+
+data = pd.read_csv("data/OnionOrNot.csv")
+
+print(data)
