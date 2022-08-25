621ab558ca207366909ecec8780864014e95d720
Arjun Suresh Kumar <Arjun Suresh Kumar>
2022-08-25 14:22:03 -0700

    model structure

--- main.py
+++ main.py
@@ -7,16 +7,15 @@ data = pd.read_csv("data/OnionOrNot.csv")
 
 tokens = Tokenizer()
 tokens.fit_on_texts(data.text)
-#print(tokens.word_counts)
-#print(tokens.word_index)
 
 def remove_high_freq(l, thresh):
     return([i for i in l if i > thresh])
 
-
+def remove_low_freq(l, thresh):
+    return([i for i in l if i < thresh])
 
 data.text = tokens.texts_to_sequences(data.text)
 data.text = data.text.map(lambda x: remove_high_freq(x, 100))
+data.text = data.text.map(lambda x: remove_low_freq(x, len(tokens.word_counts)-3000))
 
-print(len(tokens.word_index))
 print(data)
