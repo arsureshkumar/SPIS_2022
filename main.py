cedaa7f91263774323723d7e68c53b39201c2519
SkylerCrook <jskycrook@gmail.com>
2022-08-31 10:20:27 -0700

    change and found shape of data and used flatten layer

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from keras.preprocessing.text import Tokenizer
+from keras_preprocessing.sequence import pad_sequences
 from keras.models import Sequential
-from keras.layers import Dense, Embedding, Conv1D
+from keras.layers import Dense, Embedding, Conv1D, Flatten
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
 
-#data = pd.read_csv("data/OnionOrNot.csv")
-#First Dataset, to use un-comment line 8 and comment lines 30-33
-
+data = pd.read_csv("data/OnionOrNot.csv")
+#First Dataset, to use un-comment line 9 and comment lines 22-23
 
 #Convert True and False datasets to pandas, modify, then combine
 tdata = pd.read_csv("data/True.csv") #True data
@@ -19,14 +19,14 @@ fdata["label"] = 1 #adds label 1 to false data
 for i in [tdata, fdata]:
     i["text"] = i["title"].astype(str) + " " + i["body"] #Creates text column which is ombination of title and body
 
-data = [i.drop(['subject', 'title', 'body'], axis=1) for i in [tdata, fdata]] #drops unecessary data
+#data = [i.drop(['subject', 'title', 'body'], axis=1) for i in [tdata, fdata]] #drops unecessary data
+#data = pd.concat(data, ignore_index = True) #combines true and false datasets into data
 
-data = pd.concat(data, ignore_index = True) #combines true and false datasets into data
 data = data.sample(frac=1) #randomizes data
 
 
 tokens = Tokenizer()
-tokens.fit_on_texts(data.text)
+tokens.fit_on_texts(data.text.values)
 
 def remove_high_freq(l, thresh):
     return([i - thresh if i > thresh else 0 for i in l ])
@@ -37,38 +37,31 @@ def remove_low_freq(l, thresh):
 vocabulary = len(tokens.word_index)
 print(vocabulary)
 
-data.text = tokens.texts_to_sequences(data.text)
 #data.text = data.text.map(lambda x: remove_high_freq(x, 100))
 #data.text = data.text.map(lambda x: remove_low_freq(x, vocabulary-3000))
+#x = np.asarray(list(data.text)).astype('float32')
+#y = np.asarray(list(data.label)).astype('float32')
 
-print(data)
-
-
-maximum_length = len(max(data.text, key=len))
-def add_zeroes(l):
-    while len(l) < maximum_length:
-        l.append(0)
-    return(l)
-data.text = data.text.map(add_zeroes)
-
-
-x = np.asarray(list(data.text)).astype('float32')
-y = np.asarray(list(data.label)).astype('float32')
+x = tokens.texts_to_sequences(data.text.values) #instead of writing "data.text = ...", write "x = ... and use .values"
+x = pad_sequences(x) #replaces add_zeroes, pads lengths of x out to longest list
+y = data.label
 
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
 X_train, X_val,  y_train, y_val = train_test_split(X_train, y_train, test_size = 0.2)
 
 
 model = Sequential()
-model.add(Embedding(vocabulary, 64))
-model.add(Conv1D(filters=32, kernel_size=8, activation='relu'))
-model.add(Conv1D(filters=32, kernel_size=8, activation='relu')) # added another convolutional layer to improve context detection
+model.add(Embedding(vocabulary+1, 64, input_length = X_train.shape[1])) #input_length tells model the shape of the data, When 1 isnt added to vocabulary it always throws an out of bounds error so that was my short term fix lol
+model.add(Conv1D(filters=32, kernel_size=8, activation='relu')) #Code works with or without, I cant tell if it changes anything
+#model.add(Conv1D(filters=32, kernel_size=8, activation='relu')) # added another convolutional layer to improve context detection
+model.add(Flatten())
 model.add(Dense(10, activation='relu'))
 model.add(Dense(1, activation='relu'))
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 model.fit(X_train, y_train, batch_size = 32, epochs = 5, validation_data = (X_val, y_val))
+model.summary()
 
 results = model.evaluate(X_test, y_test, batch_size=128)
 print("test loss, test acc:", results)
