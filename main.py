31671dcfc89962113dc0da66a8dc7b611443e0bf
Arjun Suresh Kumar <Arjun Suresh Kumar>
2022-09-01 22:30:34 +0200

    cleanup

--- main.py
+++ main.py
@@ -9,32 +9,29 @@ import numpy as np
 data = pd.read_csv("data/OnionOrNot.csv")
 #Our Dataset
 
-data = data.sample(frac=1) #randomizes data
-
+#data preprocessing
 tokens = Tokenizer() #tokenizes dataset
 tokens.fit_on_texts(data.text.values)
-
 vocabulary = len(tokens.word_index)
-print(vocabulary)
-
 x = tokens.texts_to_sequences(data.text.values)
 x = pad_sequences(x) #pads lengths of x out to longest list
 y = data.label
 
+#split data into training, test, and validation
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
 X_train, X_val,  y_train, y_val = train_test_split(X_train, y_train, test_size = 0.2)
 
-
+#build model
 model = Sequential()
 model.add(Embedding(vocabulary+1, 64, input_length = X_train.shape[1])) #input_length tells model the shape of the data
 model.add(Flatten())
 model.add(Dense(10, activation='relu'))
 model.add(Dense(1, activation='relu'))
-
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
+#fit model
 model.fit(X_train, y_train, batch_size = 32, epochs = 5, validation_data = (X_val, y_val))
-model.summary()
 
-results = model.evaluate(X_test, y_test, batch_size=128)
-print("test loss, test acc:", results)
+#evaluate model
+model.summary()
+print("test loss, test acc:", model.evaluate(X_test, y_test, batch_size=128))
