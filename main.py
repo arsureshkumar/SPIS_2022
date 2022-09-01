178d23da2ff15d5023f963d5a64c6b79e7aa8b97
SkylerCrook <jskycrook@gmail.com>
2022-09-01 13:16:17 -0700

    final code

--- main.py
+++ main.py
@@ -6,27 +6,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
 
-#data = pd.read_csv("data/OnionOrNot.csv")
-#First Dataset, to use un-comment line 9 and comment lines 22-23
-
-#Convert True and False datasets to pandas, modify, then combine
-tdata = pd.read_csv("data/True.csv") #True data
-fdata = pd.read_csv("data/Fake.csv") #False data
-
-tdata["label"] = 0 #adds label 0 to true data
-fdata["label"] = 1 #adds label 1 to false data
-
-for i in [tdata, fdata]:
-    i["text"] = i["title"].astype(str) + " " + i["body"] #Creates text column which is ombination of title and body
-print(fdata)
-#data = [i.drop(['subject', 'title', 'body'], axis=1) for i in [tdata, fdata]] #drops unecessary data
-concatdata = [tdata, fdata]
-data = pd.concat(concatdata) #combines true and false datasets into data
+data = pd.read_csv("data/OnionOrNot.csv")
+#Our Dataset
 
 data = data.sample(frac=1) #randomizes data
 
-
-tokens = Tokenizer()
+tokens = Tokenizer() #tokenizes dataset
 tokens.fit_on_texts(data.text.values)
 
 def remove_high_freq(l, thresh):
@@ -38,13 +23,8 @@ def remove_low_freq(l, thresh):
 vocabulary = len(tokens.word_index)
 print(vocabulary)
 
-#data.text = data.text.map(lambda x: remove_high_freq(x, 100))
-#data.text = data.text.map(lambda x: remove_low_freq(x, vocabulary-3000))
-#x = np.asarray(list(data.text)).astype('float32')
-#y = np.asarray(list(data.label)).astype('float32')
-
-x = tokens.texts_to_sequences(data.text.values) #instead of writing "data.text = ...", write "x = ... and use .values"
-x = pad_sequences(x) #replaces add_zeroes, pads lengths of x out to longest list
+x = tokens.texts_to_sequences(data.text.values)
+x = pad_sequences(x) #pads lengths of x out to longest list
 y = data.label
 
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
@@ -52,9 +32,7 @@ X_train, X_val,  y_train, y_val = train_test_split(X_train, y_train, test_size =
 
 
 model = Sequential()
-model.add(Embedding(vocabulary+1, 64, input_length = X_train.shape[1])) #input_length tells model the shape of the data, When 1 isnt added to vocabulary it always throws an out of bounds error so that was my short term fix lol
-model.add(Conv1D(filters=32, kernel_size=8, activation='relu')) #Code works with or without, I cant tell if it changes anything
-#model.add(Conv1D(filters=32, kernel_size=8, activation='relu')) # added another convolutional layer to improve context detection
+model.add(Embedding(vocabulary+1, 64, input_length = X_train.shape[1])) #input_length tells model the shape of the data
 model.add(Flatten())
 model.add(Dense(10, activation='relu'))
 model.add(Dense(1, activation='relu'))
